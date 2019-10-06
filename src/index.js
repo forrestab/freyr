8cac5d37787081d3f13d130ce94a368e122e1d5b
Forrest Brown <forrestabrown@gmail.com>
2019-10-06 17:52:53 -0400

    feat: take sensor readings

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+const sensor = require("node-dht-sensor").promises;
+
 require("dotenv").config();
 
+const SENSOR_CODE = 22;
+const GPIO_PORT = 4;
+
 (async () => {
-    
+    const { temperature, humidity } = await sensor.read(SENSOR_CODE, GPIO_PORT);
 })();
