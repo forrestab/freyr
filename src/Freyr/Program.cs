4baa3a0411c604307dc560926f5d1a8aaa505ec6
Forrest Brown <forrestabrown@gmail.com>
2020-09-24 07:19:44 -0400

    feat: initial dotnet project

---
+++ src/Freyr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Freyr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
